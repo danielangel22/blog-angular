c36d58593c87af3e67b7e5f8ad109b5958ac06d2
Daniel Angel <trabajos.daniel2018@gmail.com>
2021-09-05 18:35:15 -0600

    redirect 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
-import { MenuComponent } from './components/menu/menu.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { PostsComponent } from './components/posts/posts.component';
 
